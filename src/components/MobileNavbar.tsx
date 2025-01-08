6acbaa38cb2e31b3c52919c90ad5a35a6c754025
Dinh-Thao <nguyendinhthao345@gmail.com>
2025-01-08 14:54:19 +0700

    fix mobile navbar to profile

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -15,8 +15,10 @@ import { useState } from "react";
 import { useAuth, SignInButton, SignOutButton } from "@clerk/nextjs";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { currentUser } from "@clerk/nextjs/server";
 
-function MobileNavbar() {
+async function MobileNavbar() {
+  const user = await currentUser()
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const { isSignedIn } = useAuth();
   const { theme, setTheme } = useTheme();
@@ -52,7 +54,7 @@ function MobileNavbar() {
               </Link>
             </Button>
 
-            {isSignedIn ? (
+            {(isSignedIn && user)  ? (
               <>
                 <Button variant="ghost" className="flex items-center gap-3 justify-start" asChild>
                   <Link href="/notifications">
@@ -61,7 +63,9 @@ function MobileNavbar() {
                   </Link>
                 </Button>
                 <Button variant="ghost" className="flex items-center gap-3 justify-start" asChild>
-                  <Link href="/profile">
+                  <Link href={`/profile/${
+                user.username ?? user.emailAddresses[0].emailAddress.split("@")[0]
+              }`}>
                     <UserIcon className="w-4 h-4" />
                     Profile
                   </Link>
